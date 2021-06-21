20514376052936866451466cf60ebb482e4f3840
cvegaies <58568197+cvegaies@users.noreply.github.com>
2021-06-21 10:33:59 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 let data = { "temas": [
-    {"tema": 1, "titulo": "Título tema 1", "archivos": 2},
+    {"tema": 1, "titulo": "Título tema 1", "archivos": 3},
     {"tema": 2, "titulo": "Título tema 2",  "archivos": 1},
    ]
  };
