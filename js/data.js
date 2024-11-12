5cd8231902982f4ba325204f53ed9c0fcd4315de
cvegaies <58568197+cvegaies@users.noreply.github.com>
2024-11-12 20:59:50 +0100

    New data.js

--- js/data.js
+++ js/data.js
@@ -1,5 +1,18 @@
-let data = { "temas": [
-    {"tema": 1, "titulo": "Rutas", "archivos": 3},
-    {"tema": 2, "titulo": "Vistas",  "archivos": 1},
-   ]
- };
+let data = { "temas":
+    [
+        {"tema": 1, "titulo": "Rutas", "archivos": 3},
+        {"tema": 2, "titulo": "Vistas",  "archivos": 1},
+        {"tema": 3, "titulo": "Controladores",  "archivos": 1},
+        {"tema": 4, "titulo": "Modelos",  "archivos": 1},
+        {"tema": 5, "titulo": "Migraciones",  "archivos": 1},
+        {"tema": 6, "titulo": "Request",  "archivos": 1},
+        {"tema": 7, "titulo": "Session",  "archivos": 1},
+        {"tema": 8, "titulo": "Validación",  "archivos": 1},
+        {"tema": 9, "titulo": "Files upload",  "archivos": 1},
+        {"tema": 10, "titulo": "Middleware",  "archivos": 1},
+        {"tema": 11, "titulo": "Eloquent",  "archivos": 1},
+        {"tema": 12, "titulo": "DB",  "archivos": 1},
+        {"tema": 13, "titulo": "PDO",  "archivos": 1},
+        {"tema": 14, "titulo": "Paginación",  "archivos": 1},
+    ]
+};
